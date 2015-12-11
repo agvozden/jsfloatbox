b183e2593d36da40b43f53811698b21807f5d127
agvozden <agvozden@gmail.com>
2015-12-11 17:58:39 +0100

    minimized version

--- floatbox.jquery.js
+++ floatbox.jquery.js
@@ -11,48 +11,48 @@
 			msg: null,
 			ajax: false,
 			page: false,
-			_fb: 'floatbox',
-			_fb_close: 'floatbox_close',
-			_fb_frame: 'floatbox_frame',
-			_fb_content: 'floatbox_content',
-			_fb_frame_class: 'rounded-corners',
-			_fb_overlay: false, // 'overlay',
+			_flb: 'floatbox',
+			_flb_close: 'floatbox_close',
+			_flb_frame: 'floatbox_frame',
+			_flb_content: 'floatbox_content',
+			_flb_frame_class: 'rounded-corners',
+			_flb_overlay: false, // 'overlay',
 			box: function(div, msg){
 			
 				if (msg===null) msg = div.html();			
 				
-				fHTML = '<div id="'+defaults._fb+'">'
-					+ '<a id="'+defaults._fb_close+'"></a>'
-					+ '<div id="'+defaults._fb_frame+'" class="'+defaults._fb_frame_class+'">'
-					+ '<div id="'+defaults._fb_content+'">'+msg+'</div></div></div>';
+				fHTML = '<div id="'+defaults._flb+'">'
+					+ '<a id="'+defaults._flb_close+'"></a>'
+					+ '<div id="'+defaults._flb_frame+'" class="'+defaults._flb_frame_class+'">'
+					+ '<div id="'+defaults._flb_content+'">'+msg+'</div></div></div>';
 				
-				if (defaults._fb_overlay){
-					if ($("."+defaults._fb_overlay).length==0) 
-						fHTML = '<div class="'+defaults._fb_overlay+'"></div>' + fHTML;
+				if (defaults._flb_overlay){
+					if ($("."+defaults._flb_overlay).length==0) 
+						fHTML = '<div class="'+defaults._flb_overlay+'"></div>' + fHTML;
 				}
 				
-				if ($("#"+defaults._fb).length==0){
+				if ($("#"+defaults._flb).length==0){
 					if (msg) $("body").append(fHTML);
 					else div.append(fHTML);				
 				} else {
-					$("#"+defaults._fb_content).html(msg);
-					$("#"+defaults._fb).show();
+					$("#"+defaults._flb_content).html(msg);
+					$("#"+defaults._flb).show();
 				}
 				
-				if (defaults._fb_overlay) $("."+defaults._fb_overlay).fadeIn();		
+				if (defaults._flb_overlay) $("."+defaults._flb_overlay).fadeIn();		
 				
-				$("#"+defaults._fb).on("dblclick", function(){
+				$("#"+defaults._flb).on("dblclick", function(){
 					$(this).hide();
-					if (defaults._fb_overlay) $("."+defaults._fb_overlay).fadeOut();
+					if (defaults._flb_overlay) $("."+defaults._flb_overlay).fadeOut();
 				});
-				$("#"+defaults._fb_close).on("click", function(){
-					$("#"+defaults._fb).hide();
-					if (defaults._fb_overlay) $("."+defaults._fb_overlay).fadeOut();
+				$("#"+defaults._flb_close).on("click", function(){
+					$("#"+defaults._flb).hide();
+					if (defaults._flb_overlay) $("."+defaults._flb_overlay).fadeOut();
 				});	
 				return true;
 			},
 			boxajax: function(div, url){
-				$("#"+defaults._fb_content).load(url);									
+				$("#"+defaults._flb_content).load(url);									
 			},
 		
 		};
